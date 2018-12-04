7998c148016f3d6c1a1e35eca0d1176c86c05f59
kmbzdyk <kmbzdyk@d10140.einet.brad.ac.uk>
2018-12-04 16:52:36 +0000

    update

---
+++ src/uk/ac/bradford/spacegame/Laser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.bradford.spacegame;
+
+/**
+ *
+ * @author kmbzdyk
+ */
+public class Laser extends Entity{
+    public Laser(int x, int y) {
+        setPosition(x, y);
+    }
+}
