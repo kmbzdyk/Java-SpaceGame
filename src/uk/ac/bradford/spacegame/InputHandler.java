48a8b9e21167432b10b110dce37cada059f59e1c
klaudiabzdyk <klaudiabzdyk@192.168.0.76>
2018-12-05 13:56:39 +0000

    blasters again

--- src/uk/ac/bradford/spacegame/InputHandler.java
+++ src/uk/ac/bradford/spacegame/InputHandler.java
@@ -45,7 +45,7 @@ public class InputHandler implements KeyListener {
             case KeyEvent.VK_RIGHT: engine.movePlayerRight(); break;//handle right arrow
             case KeyEvent.VK_UP: engine.movePlayerUp(); break;      //handle up arrow
             case KeyEvent.VK_DOWN: engine.movePlayerDown(); break;  //handle down arrow
-            case KeyEvent.VK_CAPS_LOCK: engine.blastersOn(); break; //booom
+            case KeyEvent.VK_SHIFT: engine.blastersOn(); break; //booom
         }
         engine.doTurn();    //any key press will result in this method being called
     }
