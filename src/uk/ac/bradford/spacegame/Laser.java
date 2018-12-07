9d097b1f1a8e4231af5696232188b3d1e678fefd
kmbzdyk <kmbzdyk@d10140.einet.brad.ac.uk>
2018-12-07 15:41:33 +0000

    almost done

--- src/uk/ac/bradford/spacegame/Laser.java
+++ src/uk/ac/bradford/spacegame/Laser.java
@@ -6,10 +6,18 @@
 package uk.ac.bradford.spacegame;
 
 /**
- *
+ * The Laser class extends the Entity class.
+ * Objects of this class are used to
+ * store the position of lasers in the game.
  * @author klaudiabzdyk
  */
 public class Laser extends Entity{
+    /**
+     * Creates an Laser object with specified
+     * position on the game board.
+     * @param x Starting X position for the laser.
+     * @param y Starting Y position for the laser.
+     */
     public Laser(int x, int y) {
         setPosition(x, y);
     }
