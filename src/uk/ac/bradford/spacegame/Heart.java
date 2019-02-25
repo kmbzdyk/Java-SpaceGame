a118b1f69be0e094dbcf78be79a83f630ab91733
klaudiabzdyk <klaudiabzdyk@192.168.0.76>
2019-02-25 18:33:54 +0000

    added heart object

---
+++ src/uk/ac/bradford/spacegame/Heart.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.bradford.spacegame;
+
+/**
+ *
+ * @author klaudiabzdyk
+ */
+public class Heart extends Entity {
+    
+    /**
+     * 
+     * @param x Starting X position for the alien.
+     * @param y Starting Y position for the alien.
+     */
+    public Heart(int x, int y) {
+        setPosition(x, y);
+    }
+}
